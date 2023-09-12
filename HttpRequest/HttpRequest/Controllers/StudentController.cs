bd414ac618e42682768f91f41df446ecd011d004
vivekkumarkar@gmail.com <vivekkumarkar@gmail.com>
2023-09-13 00:16:46 +0530

    first commit

---
+++ HttpRequest/HttpRequest/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using HttpRequest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HttpRequest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private static readonly List<Student> students = new List<Student>
+        {
+            new Student { Name = "Walter", Class = "7A", Address = "Address 1", Email = "walter@email.com" },
+            new Student { Name = "Matthew", Class = "7A", Address = "Address 2", Email = "matthew@email.com" },
+            new Student { Name = "Anne", Class = "7A", Address = "Address 3", Email = "anne@email.com" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return students;
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<Student> GetStudent(string name)
+        {
+            var student = students.FirstOrDefault(p => p.Name == name);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+    }
+}
---
+++ HttpRequest/HttpRequest/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace HttpRequest.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+    }
+}
